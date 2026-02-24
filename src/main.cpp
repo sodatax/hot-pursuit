3ecbc32c0b62bdec0236a868c1b6a327a9877a40
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-02-24 19:50:10 +0000

    added enemy constructor

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ public:
         size(player_size),
         bounding_box(create_bounding_box(sprite, size))
     {}
-    
+
     /**
      * Update the position and bounding box of the player based on d-pad movement.
      */
@@ -146,6 +146,32 @@ public:
     bn::rect bounding_box; // The rectangle around the sprite for checking collision
 };
 
+class Enemy
+{
+public:
+    Enemy(int starting_x, int starting_y, bn::fixed enemy_speed, bn::size enemy_size) : 
+        enemy_sprite(bn::sprite_items::square.create_sprite(starting_x, starting_y)),
+        speed(enemy_speed),
+        size(enemy_size),
+        bounding_box(create_bounding_box(enemy_sprite, size))
+    {}
+
+    /**
+     * Update the position and bounding box of the player based on d-pad movement.
+     */
+    void update()
+    {
+
+        bounding_box = create_bounding_box(enemy_sprite, size);
+    }
+
+    // Create the sprite. This will be moved to a constructor
+    bn::sprite_ptr enemy_sprite;
+    bn::fixed speed;       // The speed of the player
+    bn::size size;         // The width and height of the sprite
+    bn::rect bounding_box; // The rectangle around the sprite for checking collision
+};
+
 int main()
 {
     bn::core::init();
@@ -156,16 +182,15 @@ int main()
     // Create a player and initialize it
     // TODO: we will move the initialization logic to a constructor.
     Player player = Player(35, 22, 7, PLAYER_SIZE);
-
-    bn::sprite_ptr enemy_sprite = bn::sprite_items::square.create_sprite(-30, 22);
-    bn::rect enemy_bounding_box = create_bounding_box(enemy_sprite, ENEMY_SIZE);
+    Enemy enemy = Enemy(-30, 22, 2, ENEMY_SIZE);
 
     while (true)
     {
         player.update();
+        enemy.update();
 
         // Reset the current score and player position if the player collides with enemy
-        if (enemy_bounding_box.intersects(player.bounding_box))
+        if (enemy.bounding_box.intersects(player.bounding_box))
         {
             scoreDisplay.resetScore();
             player.sprite.set_x(44);
